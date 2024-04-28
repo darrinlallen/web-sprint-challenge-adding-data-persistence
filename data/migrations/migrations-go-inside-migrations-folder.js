06089117019f7330fae751ccca45eec290cd62b2
Darrin <darrin.lovard.allen@gmail.com>
2024-04-28 16:36:26 -0700

    added routing fnctionality

--- data/migrations/migrations-go-inside-migrations-folder.js
+++ data/migrations/migrations-go-inside-migrations-folder.js
@@ -1,11 +1,9 @@
-Use the `data/migrations` folder to keep your migration(s) as per your knexfile
 
 exports.up = function (knex) {
   return knex.schema
     .createTable('projects', tbl => {
       tbl.increments('project_id');
-      tbl.string('project_name', 128)
-        .required();
+      tbl.string('project_name', 128).required;
       tbl.string('project_description', 128);
       tbl.string('project_completed', 5)
       .notNullable().defaultTo('false')
@@ -14,12 +12,12 @@ exports.up = function (knex) {
     // we can chain together createTable
     .createTable('resources', tbl => {
       tbl.increments('resource_id');
-      tbl.string('resource_name', 128).required().unique();
+      tbl.string('resource_name', 128).required;
       tbl.string('resource_description');
     })
     .createTable('tasks', tbl => {
       tbl.increments('task_id');
-      tbl.string('task_description', 128).required();
+      tbl.string('task_description', 128).required;
     tbl.string('task_notes', 128);
     tbl.string('task_completed', 5).notNullable().defaultTo('false');
      tbl.integer('project_id')
